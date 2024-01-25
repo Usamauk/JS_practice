bfa550ff85a3e0debc12415c1950e2f8f716dcab
Usamauk <51535170+Usamauk@users.noreply.github.com>
2024-01-25 15:37:19 +0500

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,11 @@
+//  Area of rectangle
+
+// let len = (2);
+// let wid = (4);
+// let area = (len * wid);
+// console.log("Area of Rectangular is " + area); 
+
+
+
+// let user_name = prompt("your name plz:");
+// alert("wellcome " + user_name);
