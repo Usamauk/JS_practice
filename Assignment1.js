e121faa12deda2c5f8a419c7e4a0d654e5567100
Usamauk <51535170+Usamauk@users.noreply.github.com>
2024-01-25 15:32:40 +0500

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,8 @@
+// let name = ("usama");
+// let age = (22);
+// console.log("your name is " + name + " and your age is " + age);
+
+// let user = prompt("Enter your name");
+// alert("wellcome " + user + "Thank u for coming my web page")
+
+
