3e679ebae29c38e1468bf1488855aa3c936fca60
Usamauk <51535170+Usamauk@users.noreply.github.com>
2024-01-29 16:37:24 +0500

    Delete Assignment4.js

--- Assignment4.js
+++
@@ -1,5 +0,0 @@
-//                 // Table
-// let user_num = +prompt("Enter your number Do u want print table");
-// for (let i=1 ; i<=10; i++) {
-//     console.log(user_num + "X" + i + "=" + user_num * i);
-// }
