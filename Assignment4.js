d21ccdcb1b25ae3a4074a2274c1a5d226cf1561f
Usamauk <51535170+Usamauk@users.noreply.github.com>
2024-01-29 13:06:15 +0500

    table

---
+++ Assignment4.js
@@ -0,0 +1,5 @@
+//                 // Table
+// let user_num = +prompt("Enter your number Do u want print table");
+// for (let i=1 ; i<=10; i++) {
+//     console.log(user_num + "X" + i + "=" + user_num * i);
+// }
