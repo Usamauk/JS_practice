b526c814fe8b80dc806dce36f6b51e32a11e6ee3
Usamauk <51535170+Usamauk@users.noreply.github.com>
2024-01-29 12:45:24 +0500

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,55 @@
+// let user_temp = +prompt("Enter the current temperature in Celsius");
+// if (user_temp > 30) {
+//     alert("it's a hot day!");
+// }
+// else {
+//     alert("The weather is moderate.")
+
+
+// }
+
+
+// console.log("Marksheet");
+// let Std_name = prompt("Name:");
+// let math = +prompt("Math:");
+// let phy = +prompt("Physics:");
+// let Eng = +prompt("English:");
+// let urdu = +prompt("Urdu:");
+// let isl = +prompt("Islamiat:");
+// let total_marks = 500;
+// let num = math + phy + Eng + urdu + isl ;
+// console.log("Name:" + Std_name);
+// console.log("Max_marks:" + total_marks);
+// console.log("Math:" + math);
+// console.log("physics:" + phy);
+// console.log("English:" + Eng);
+// console.log("urdu:" + urdu);
+// console.log("islamiat:" + isl);
+// console.log("Total Marks:" + num);
+// let grade = num / total_marks * 100;
+// if (grade >=80) {
+//     console.log("you grade is A one");
+// }
+// else if (grade >=70 && grade <=79) {
+//     console.log("your grade is A");
+// }
+// else if (grade >=60 && grade <= 69 ) {
+//     console.log("your grade is B");
+// }
+// else if (grade >=50 && grade <=59){
+//     console.log("your grade is C");
+// }
+// else if (grade >35 && grade <=49){
+//     console.log("your grade is D");
+// } 
+// else {
+//     console.log("your grade is F and your percentage is less then 35");
+// }
+
+
+
+
+// let total_marks = 500;
+// let subjects = 5;
+// let Each_sub = ("Each subject carry 100 marks");
+
