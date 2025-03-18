7cf64907468620924d4b49094fa019f2c75d7972
Usamauk <51535170+Usamauk@users.noreply.github.com>
2025-03-18 22:31:41 +0000

    Start to Array Method

--- practice01.js
+++ practice01.js
@@ -1,1 +1,80 @@
-console.log("usama khan");
+// const a = "usamakhan";
+// let b = "daniyal";
+// var c = "moiz";
+// let d;
+// a = "danoyal";
+// b = "usama";
+// c = "moix khano"
+
+// var  use nh krna wo scope ko nh janta 
+
+// console.table([a,b,c,d])
+
+"use strict"; //new version use krne ke leye
+
+// Data types
+
+// let a = "usama" // string
+// let b = 23      // number
+// let c = false; // bolean true/false
+// let d;
+// let e = null;
+
+// console.table(typeof a);
+// console.table(typeof b);
+// console.table(typeof c);
+// console.table(typeof d);
+// console.table(typeof e);
+
+//convert different data types
+// "33" => 33
+//"33abc" => NaN string number m convert nh ho sakta 
+
+// console.log(Number (e));
+
+//*************************** Operations ***************************
+// Arithematic operator
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3%3);
+// console.log(3**3);
+
+// Comparign operator
+// >
+// <
+// ==
+// !==
+// >=
+// <=
+// console.log(3>3);
+// console.log(3<3);
+// console.log(3>=3);
+// console.log(3<=3);
+// console.log(3===3);
+// console.log(3!==3);
+
+// stuck (premetive) copy m change hota       heap (non- premitive) real m change
+// string,number,null,undefined,symbol,       Array,object,function
+// boolean,biginit
+
+
+// use template leterial concatinate ke leye use krte
+
+// let u = "usama"
+// let b = 23
+// console.log(`my name is ${u} khan and my age is ${b}`);
+
+// string method
+// let u = "usamakhan ";
+// console.log(u.at(3));
+// console.log(u.endsWith("khan"));
+// console.log(u.length);
+// console.log(u.repeat(3));
+
+
+
+
+
+
