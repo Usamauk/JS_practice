6fb98d12a12bd95f146bfa359491eefbd82ebd3f
Usamauk <51535170+Usamauk@users.noreply.github.com>
2025-03-21 22:54:49 +0000

    Number/Math/Date/Array

---
+++ practice02.js
@@ -0,0 +1,64 @@
+// Number and Math
+
+// let u = new Number(1000000);
+// console.log(u.toFixed(2));
+// console.log(u.toLocaleString('en-IN'));
+// console.log(u.toPrecision(5));
+// console.log(u.toString().length);
+ 
+
+// ********************** Math ***********************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-3));
+
+// console.log( Math.round(4.5));
+// console.log( Math.floor(4.5));
+// console.log( Math.ceil(4.6));
+// console.log( Math.max(2,3,4,5,6,7,9,10));
+// console.log( Math.min(2,3,4,5,6,7,9,10));
+// console.log(Math.round(Math.random() *10) + 1);
+
+// let min = 10;
+// let max = 20;
+// console.log(Math.random() * ((max - min) + 1) + min);
+
+
+// ********************************** Date and  Time ****************************
+
+// 1 january 1970 se date and time start krta
+
+// console.log(Date);
+// let u = new Date();
+// console.log(u.toString());
+// console.log(u.toLocaleTimeString());
+// console.log(u.toLocaleString());
+
+// console.log(u.toLocaleString('default',{
+//     weekday: "long",
+//     day : "2-digit",
+//     year : "numeric"
+// }));
+
+
+// console.log(u.set("2023-01-12"));
+
+
+// **************************** Array in js ****************************
+
+let arr = [1,2,3,4,5,6,"usama"];
+arr.push("daniyal")
+arr.pop()
+arr.shift()
+arr.unshift("daniyal")
+// arr.values()
+// arr.includes("umair")
+
+// from , of ,
+
+console.log(arr.slice(1,3));
+console.log(arr.splice());
+
+
+
