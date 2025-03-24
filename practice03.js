14f8b0e3ee6bab1bfab5e3068eddf8f1c8c4d267
Usamauk <51535170+Usamauk@users.noreply.github.com>
2025-03-24 22:05:04 +0000

    Array / object

---
+++ practice03.js
@@ -0,0 +1,118 @@
+// let u = ["usama","daniyal","moiz","maaz","saad"];
+// let k = ["danoyal","kaka"];
+// u.flat(Infinity);
+// console.log(u);
+
+// slice array ke andr de nikal kr deta
+// splice array ko manupilate krta he 
+
+// let u = ["usama","daniyal","moiz","maaz","saad"];
+// let k = ["danoyal","kaka"];
+// let s = [...u , ...k];
+// console.log(s);
+
+
+
+// let u = Array.isArray('usama');
+// console.log(u);
+
+
+// let u = Array.from('usama');
+// console.log(u);
+
+
+// let u = 100;
+// let k = 200;
+
+// let s = Array.of(u,k)
+// console.log(s);
+
+// ************************************ object ************************************
+
+// singleton object
+// Object.create({})
+
+
+// object laterial
+// let obj = {
+//     name: "usama",
+//     f_name : "Faisal",
+//     DOB : "13-09-2000"
+// }
+
+// console.log(obj["f_name"]);   // prefer this  type for  access object
+// console.log(obj.name);
+
+// Object.freeze(obj)
+// obj.DOB = '13-08-2000'
+
+// console.log(obj);
+
+
+// let u = {
+//     name : "usama"
+// }
+
+// let k = {
+//     f_name : "faisal",
+//     name : "usama"
+// }
+
+// let s = Object.assign(u,k)
+// console.log(s);
+
+// let s = {...u , ...k}
+// console.log(s);
+
+
+// let obj = [
+//     {
+//         name: "usama",
+//         f_name : "faisal"
+//     },
+
+//     {
+//         name: "daniyal",
+//         f_name : "faisal" 
+         
+//     },
+
+//     {
+//         name: "kaka",
+//         f_name : "faisal" 
+         
+//     },
+
+//     {
+//         name: "maaz",
+//         f_name : "faisal" 
+         
+//     },
+
+// ]
+
+// let u = obj[1].name;
+// console.log(u);
+
+
+let obj = 
+    {
+        name: "usama",
+        f_name : "faisal"
+    }
+
+// let u = Object.keys(obj)
+// let k = Object.values(obj)
+// let l = Object.entries(obj)
+// console.log(u,k,l);
+
+// let {f_name: usama} = obj;
+// console.log(usama);
+
+
+// json ko json formetter ke zarye theek se convert krte object me 
+
+
+
+
+
